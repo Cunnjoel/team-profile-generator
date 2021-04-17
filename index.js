94019bbfc36a0ee78fbb9498d70e5d28a6b76d61
Cunnjoel <cunningham.joel84@gmail.com>
2021-04-16 22:45:03 -0500

    uploading current js

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-    .prompt({
-        
-    })
+    .prompt([
+        {
+            type: 'checkbox',
+            message: 'What job will you be performing here?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?', 
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID number?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your E-mail address?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office number?',
+            when: (answers) => answers.role === "Manager"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+            when: (answers) => answers.role === "Engineer"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the name of you school?',
+            when: (answers) => answers.role === "Intern"
+        }
+    ])
